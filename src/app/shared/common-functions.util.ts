d2e20a5b917d9028f43c3ab779e83fbf4ed29a3f
IJustDev <alexanderpanovi@yahoo.de>
2019-09-05 20:01:31 +0200

    Removing dynamic base hrefs

--- src/app/shared/common-functions.util.ts
+++
@@ -1,5 +0,0 @@
-export function getBaseLocation() {
-    let paths: string[] = location.pathname.split('/').splice(1, 1);
-    let basePath: string = (paths && paths[0]) || 'my-account'; // Default: my-account
-    return '/' + basePath;
-}
